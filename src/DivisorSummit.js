12320560be0b2877f0402efccb5eee36e66251ff
Rayched <ny2401@naver.com>
2024-02-16 18:28:49 +0900

    약수의 합 src, report 추가

--- src/DivisorSummit.js
+++ src/DivisorSummit.js
@@ -1,1 +1,16 @@
 //Programmers LV1 약수의 합
+
+function Solution(n){
+    let answer = 0;
+
+    for (let i = 1; i <= n; i++){
+        if (n % i == 0){
+            answer += i;
+        }
+    }
+
+    console.log(answer);
+}
+
+Solution(12);
+Solution(5);
